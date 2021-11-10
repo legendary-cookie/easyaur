70ee5db340e2635ff4b179186cdefa3e1278fae8
legendary-cookie <github.com@xolley.de>
2021-11-10 16:58:16 +0100

    update build

--- build.sh
+++ build.sh
@@ -45,12 +45,12 @@ if [[ -d "repos" ]]; then
 	cd repos
 	for f in **/PKGBUILD; do
 		cd $(echo $f|sed 's/PKGBUILD//g')
-		makepkg -sf --nocheck --skippgpcheck
+		makepkg -sf --nocheck --skippgpcheck --sign
 		cp  *.pkg.tar.* $ROOT/out
 	done
 fi
 
 if [[ -f "PKGBUILD" ]]; then
-	makepkg -sf --nocheck --skippgpcheck
+	makepkg -sf --nocheck --skippgpcheck --sign
 	cp  *.pkg.tar.* $ROOT/out
 fi
