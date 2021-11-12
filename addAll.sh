bb698122bf16b39b1c1eddb2232fe774a24e5f82
legendary-cookie <github.com@xolley.de>
2021-11-12 17:38:47 +0100

    Merge clean.sh and addAll.sh into easyaur

--- addAll.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for f in $(find $HOME/.pkgbuilder/out/ -type f ! -name "*.sig")
-do
-	add2repo $f
-done
--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for f in $(find . -maxdepth 1 -type d)
-do
-	[[ $f == "." ]] && continue
-	[[ $f == *".git" ]] && continue
-	rm -rf $f
-done
